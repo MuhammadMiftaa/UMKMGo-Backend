3e6e913735b5457bf225dad54aa20f7b2209d3e9
Muhammad Mifta <madmifta77@gmail.com>
2025-11-10 22:47:39 +0700

    remove timezone db

--- config/db/config.go
+++ config/db/config.go
@@ -12,7 +12,7 @@ import (
 var DB *gorm.DB
 
 func SetupDatabase(cfg env.Database) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
